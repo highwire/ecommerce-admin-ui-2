fb4a9b4ffa9471a66db7ad703c51d528f67bdecd
NitinKataria06 <nitin.kataria@highwirepress.com>
2022-06-03 18:27:01 +0530

    detailed page alert condition

--- src/app/reports/detailed/detailed.component.ts
+++ src/app/reports/detailed/detailed.component.ts
@@ -41,6 +41,9 @@ export class DetailedComponent implements OnInit {
   pageSizeOptions: number[] = [5, 10, 25, 100];
   animal: any;
   name: any;
+  exporter:any;
+  
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   constructor(
