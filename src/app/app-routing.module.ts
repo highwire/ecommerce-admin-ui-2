dad4bca7805f40df56492599bffc40e6e95a51ea
NitinKataria06 <nitin.kataria@highwirepress.com>
2022-06-08 14:50:20 +0530

    path url bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Subscriber } from 'rxjs';
+import { AuthGuardService } from './auth/auth-gaurd.service';
+
 
 
 const routes: Routes = [
    { path: '', redirectTo: '/login', pathMatch: 'full' },
-   { path: 'login',loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+   { path: 'login',
+   loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
    { path: 'journals',
-   
+  canActivate:[AuthGuardService],
    loadChildren: () => import('./journals/journals.module').then(m => m.JournalsModule)},
-   { path: 'reports',loadChildren: () => import('./reports/reports.module').then(m => m.ReportsModule)},
+   { path: 'reports',
+   canActivate:[AuthGuardService],
+   loadChildren: () => import('./reports/reports.module').then(m => m.ReportsModule)},
    
    
 ];
--- src/app/auth/auth-gaurd.service.ts
+++ src/app/auth/auth-gaurd.service.ts
@@ -9,20 +9,57 @@ import {
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  
+  isLoggedIn: boolean = false;
   constructor(private router: Router,
     public base:BaseService,
     public http:HTTPService
     ) {
   }
  
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
-    var promise = new Promise((resolve, reject) => { 
-        // resolve("Promise Resolved"); 
-        var currency:any= localStorage.getItem('currency');
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+if(this.isLoggedIn){
+  return true;
+}else{
+  console.log('please login first') 
+  this.router.navigate([''])
+  return false
+}
+
+    // var promise = new Promise((resolve, reject) => { 
+    //     // resolve("Promise Resolved"); 
+    //     var currency:any= localStorage.getItem('currency');
+    // if(currency){
+    //     currency= JSON.parse(currency) ;
+    //     resolve(true); 
+    // }else{
+
+    //   let publisher = localStorage.getItem('publisher')  ;
+    //   // var name =  window.encodeURIComponent(this.basedata.element.name)
+    //   let URL= this.base.CURRENCY_LIST+publisher+ '/currencies';
+    //   this.http.getDatawithGet(URL,'').subscribe((res:any)=>{
+    //   // this.currency  =  res;
+    //   localStorage.setItem('currency',JSON.stringify(res));
+    //     console.log(res);       
+    //     resolve(true); 
+    //   })
+    // }
+    // }) 
+    
+    // promise.then((success) => { 
+    //     console.log()
+    //     this.router.navigate(['/journals/specific']);
+
+    //     return false
+    // }) 
+    
+    
+  }
+
+  getCurrencyList(){
+    var currency:any= localStorage.getItem('currency');
     if(currency){
         currency= JSON.parse(currency) ;
-        resolve(true); 
     }else{
 
       let publisher = localStorage.getItem('publisher')  ;
@@ -32,38 +69,10 @@ export class AuthGuardService implements CanActivate {
       // this.currency  =  res;
       localStorage.setItem('currency',JSON.stringify(res));
         console.log(res);       
-        resolve(true); 
       })
     }
-    }) 
-    
-    promise.then((success) => { 
-        console.log()
-        this.router.navigate(['/journals/specific']);
-
-        return true
-    }) 
-    
-    
-  }
-
-//   getCurrencyList(){
-//     var currency:any= localStorage.getItem('currency');
-//     if(currency){
-//         currency= JSON.parse(currency) ;
-//     }else{
-
-//       let publisher = localStorage.getItem('publisher')  ;
-//       // var name =  window.encodeURIComponent(this.basedata.element.name)
-//       let URL= this.base.CURRENCY_LIST+publisher+ '/currencies';
-//       this.http.getDatawithGet(URL,'').subscribe((res:any)=>{
-//       // this.currency  =  res;
-//       localStorage.setItem('currency',JSON.stringify(res));
-//         console.log(res);       
-//       })
-//     }
   
-//   }
+  }
 
 
 } 
