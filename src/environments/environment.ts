2293a5f4588e24404d21e6e2ef9699487952b684
Gurpreet <gurpreet.singh@mpslimited.com>
2022-06-14 11:26:05 +0530

    server issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,28 +10,28 @@ export const environment = {
 
   // http://10.220.52.127:5006/login
 
-  production: false,
-  appRole: 'Collections maint',
-  atomBase: 'http://10.220.52.127:5006/atom',
-  apiBase: 'http://10.220.52.127:5006/api',  
-  loginUrl: 'http://10.220.52.127:5006/login-global',
-  taxonomyUrl: 'http://10.220.52.127:5006/api/tree',
-  membershipUrl: 'http://10.220.52.127:5006/api/acs/collection-citations'
-
-
-
-
-
-  
-  
   // production: false,
   // appRole: 'Collections maint',
-  // atomBase: 'https://ecommerce-service.highwire.org/atom',
-  // apiBase: 'https://ecommerce-service.highwire.org/api',
+  // atomBase: 'http://10.220.52.127:5006/atom',
+  // apiBase: 'http://10.220.52.127:5006/api',  
+  // loginUrl: 'http://10.220.52.127:5006/login-global',
+  // taxonomyUrl: 'http://10.220.52.127:5006/api/tree',
+  // membershipUrl: 'http://10.220.52.127:5006/api/acs/collection-citations'
+
+
+
+
+
   
-  // loginUrl: 'https://ecommerce-service.highwire.org/login-global',
-  // taxonomyUrl: 'https://ecommerce-service.highwire.org/api/tree',
-  // membershipUrl: 'https://ecommerce-service.highwire.org/api/acs/collection-citations'
+  
+  production: false,
+  appRole: 'Collections maint',
+  atomBase: 'https://ecommerce-service.highwire.org/atom',
+  apiBase: 'https://ecommerce-service.highwire.org/api',
+  
+  loginUrl: 'https://ecommerce-service.highwire.org/login-global',
+  taxonomyUrl: 'https://ecommerce-service.highwire.org/api/tree',
+  membershipUrl: 'https://ecommerce-service.highwire.org/api/acs/collection-citations'
   
 };
 
